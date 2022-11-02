0150abcaa78f4ef2c630ebd04ffe51a5e9870a18
Deepak Tanwar <dktanwar@hotmail.com>
2022-11-02 10:20:01 +0100

    Update win_login_student.sh

--- win_login_student.sh
+++ win_login_student.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-winpty docker run --rm -it --entrypoint bash -v `pwd`/data:/home/student/data masaomi/biodocker
+winpty docker run --rm -v /$(pwd)/data:/home/student/data --user student --hostname biodocker --name biodocker -ti masaomi/biodocker bash --login
