e0e4894f5b4e3fba582b7095b347f70b0f100ac9
Deepak Tanwar <dktanwar@hotmail.com>
2023-11-07 18:31:20 +0100

    updated script for windows

--- win_login_student.sh
+++ win_login_student.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-winpty docker run --rm -v /$(pwd)/data:/home/student/data --user student --hostname biodocker --name biodocker -ti dktanwar/bio_609_610 bash --login
+command=`docker container ls -a|grep biodocker`
+
+if [ -z "$command" ]; then
+  winpty docker run -v `pwd`/data:/home/student/data --user student --hostname biodocker --name biodocker -ti dktanwar/bio_609_610 bash --login
+else
+  winpty docker container start biodocker
+  winpty docker exec -it --user student biodocker bash
+fi
