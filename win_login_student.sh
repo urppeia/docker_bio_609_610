092e367827fabf38105e1c5036576e8c827f3724
Deepak Tanwar <dktanwar@hotmail.com>
2024-12-06 06:54:58 +0100

    Update win_login_student.sh

--- win_login_student.sh
+++ win_login_student.sh
@@ -3,7 +3,7 @@
 command=`docker container ls -a|grep biodocker`
 
 if [ -z "$command" ]; then
-  winpty docker run -v `pwd`/data:/home/student/data --user student --hostname biodocker --name biodocker -ti dktanwar/bio_609_610 bash --login
+  winpty docker run --rm -v /$(pwd)/data:/home/student/data --user student --hostname biodocker --name biodocker -ti dktanwar/bio_609_610  bash --login
 else
   winpty docker container start biodocker
   winpty docker exec -it --user student biodocker bash
